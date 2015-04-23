4ac4dcbc980815851bf602ce73c15f20388e0171
lyrl <184387904@qq.com>
2015-04-23 15:04:59 +0800

    change

---
+++ google_translator/GoogleConmunication.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 与google服务器通讯类
+import urllib2
+import urllib
+from Lang import *
+
+
+class GoogleConmunication(object):
+    # 发送翻译请求到google服务器，返回翻译的内容
+    # @ParamType sourceLang string
+    # 源语言
+    # @ParamType targetLang string
+    # 目标语言
+    # @ParamType content string
+    # 待翻译的内容
+    # @ReturnType string
+    def translate(self, source_lang=Lang.auto, target_lang=Lang.chinese_simplified, content=None):
+        if not content:
+            return content
+
+        data = {"client": "t", "sl": source_lang, "tl": target_lang, "dt": "t", "ie": "UTF-8", "q": content}
+        data = urllib.urlencode(data)
+
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        req = urllib2.Request(self.__baseUrl, data, headers)
+
+        raw = urllib2.urlopen(req).read()
+
+        return self.parse_resutlt(raw)
+
+    def parse_resutlt(self, raw=None):
+        # 解析服务器返回得信息
+        # @ParamType raw string
+        # 服务器返回未经解析的数据
+        # @ReturnType string
+        if not raw:
+            return raw
+
+        index = raw.rfind(']],')
+
+        raw = raw[1:index+2]
+        result_list = eval(raw)
+
+        result = ''
+
+        for i in result_list:
+            result += i[0]
+
+        return result
+
+    def __init__(self):
+        # @AttributeType string
+        # google翻译接口地址
+        self.__baseUrl = "https://translate.google.com/translate_a/single"
+
+
+
+
+
+
---
+++ google_translator/Translator.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 翻译官
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from google_translation_api import GoogleConmunication
+import Lang
+
+
+class Translator(object):
+    # 翻译
+    # @ParamType content string
+    # 要翻译的文本
+    # @ReturnType string
+    def translate(self, content=None):
+        if not content:
+            return content
+
+        return self.__google_conmunication.translate(source_lang=self.get_source_lang(),
+                                                     target_lang=self.get_target_lang(), content=content)
+
+    # @ReturnType string
+    def get_target_lang(self):
+        return self.__target_lang
+
+    # @ParamType target_lang string
+    # @ReturnType void
+    def set_target_lang(self, target_lang):
+        self.__target_lang = target_lang
+
+    # @ReturnType string
+    def get_source_lang(self):
+        return self.__source_lang
+
+    # @ParamType source_lang string
+    # @ReturnType void
+    def set_source_lang(self, source_lang):
+        self.__source_lang = source_lang
+
+    def __init__(self, target_lang):
+        # @AttributeType string
+        # 目标语言
+        self.__target_lang = None
+        # @AttributeType string
+        # 源语言，默认为auto，程序会自动检测语言
+        self.__source_lang = "auto"
+        # @AttributeType GoogleConmunication
+        # 通讯类
+        self.__google_conmunication = GoogleConmunication.GoogleConmunication()
+
+        if target_lang:
+            self.set_target_lang(target_lang)
+
+if __name__ == "__main__":
+    content = sys.argv[1]
+    t = Translator(Lang.Lang.chinese_simplified)
+    print t.translate(content)
